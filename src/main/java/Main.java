810dfd0b92468f260f920727785811ccc9db5d65
dam2a-2021 <nmoyag01@elpuig.xeill.net>
2022-01-26 20:16:37 +0100

    Restructuración del código

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,4 @@
-import com.opencsv.CSVWriter;
-import com.opencsv.bean.StatefulBeanToCsv;
-import com.opencsv.bean.StatefulBeanToCsvBuilder;
-import com.opencsv.exceptions.CsvBadConverterException;
-import org.apache.commons.collections.ArrayStack;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxBinary;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
   // Libreria necesaria a implementar: https://www.baeldung.com/opencsv
